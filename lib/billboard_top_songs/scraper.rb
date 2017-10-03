0557c2f32e6d9df260b91b6630c5e847f7981580
Sean Clarke <xseanclarke@gmail.com>
2017-10-03 13:40:26 -0400

    Updated Scraper to account for artist as spans as well as links (features / newer artist

--- lib/billboard_top_songs/scraper.rb
+++ lib/billboard_top_songs/scraper.rb
@@ -13,7 +13,7 @@ class BillboardTopSongs::Scraper
       BillboardTopSongs::Song.new(
       "http://www.billboard.com/charts/hot-100",
       content.css("h2.chart-row__song").text,
-      content.css("div.chart-row__container > div > a.chart-row__artist").text.split(" ").join(" "),
+      content.css("div.chart-row__container > div > span.chart-row__artist", "div.chart-row__container > div > a.chart-row__artist").text.split(" ").join(" "),
       content.css("span.chart-row__current-week").text,
       content.css("div.chart-row__last-week > span.chart-row__value").text.split,
       content.css("div.chart-row__top-spot > span.chart-row__value").text.split,
